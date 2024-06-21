7462261bd3a61bdbf54e33f68d0f787fa0ae675c
brdoler <brendadoler@gmail.com>
2024-06-21 12:50:52 +0000

    solutioned

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -7,8 +7,9 @@
 
 int main(){
     std::string str;
+    std::cout << "Please enter your first name:" << std::flush;
     std::cin >> str;
-    std::cout << str;
+    std::cout << "Thank you for entering your name, " << str << "!" << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
